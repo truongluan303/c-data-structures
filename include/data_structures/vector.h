2f7f3e68193b3db52d072e71f4c2a864c5ee4b28
Luan Truong <truongluan303@gmail.com>
2023-03-26 18:23:52 -0700

    Remove trailing spaces

--- include/data_structures/vector.h
+++ include/data_structures/vector.h
@@ -32,37 +32,37 @@ vector;
 
 /******************************************************************************
  * @brief       Initialize a vector.
- * 
+ *
  * @param v             The vector to be initialized.
- * 
+ *
  * @return      0 if successful, 1 otherwise.
  *****************************************************************************/
 int construct_vector(vector* v);
 
 /******************************************************************************
  * @brief       Get the element at a given index in the vector.
- * 
+ *
  * @param v             The vector to get data from.
  * @param index         The index to get data from.
- * 
+ *
  * @return      The data at the given index.
  *****************************************************************************/
 void* vector_get(vector* v, size_t index);
 
 /******************************************************************************
  * @brief       Set the data at a given index in the vector.
- * 
+ *
  * @param v             The vector to set data.
  * @param index         The index where data will be set.
  * @param data          The data to be assigned.
- * 
+ *
  * @return      0 if successful, 1 otherwise.
  *****************************************************************************/
 int vector_set(vector* v, size_t index, void* data);
 
 /******************************************************************************
  * @brief       Add a new element to the end of the vector.
- * 
+ *
  * @param v             The vector to be added to.
  * @param data          The data to be added.
  *
@@ -82,9 +82,9 @@ int vector_pushfront(vector* v, void* data);
 
 /******************************************************************************
  * @brief       Remove the element at the end of the vector.
- * 
+ *
  * @param v             The vector to remove element from.
- * 
+ *
  * @return      The data removed.
  *****************************************************************************/
 void* vector_popback(vector* v);
@@ -100,31 +100,31 @@ void* vector_popfront(vector* v);
 
 /******************************************************************************
  * @brief       Remote the element at the given index.
- * 
+ *
  * @param v             The vector to remove the element from.
  * @param index         The index of the element to be removed.
- * 
+ *
  * @return      The value of the deleted element.
 */
 void* vector_delete(vector* v, size_t index);
 
 /******************************************************************************
  * @brief       Check if the vector contains a specific data value.
- * 
+ *
  * @param v             The vector to be searched.
  * @param data          The data to be searched.
- * 
+ *
  * @return      `TRUE` if the vector contains the given data, `FALSE`
  *              otherwise.
  *****************************************************************************/
 BOOLEAN vector_contains(vector* v, void* data);
 
 /******************************************************************************
- * @brief       Find the index of an element in a vector. 
- * 
+ * @brief       Find the index of an element in a vector.
+ *
  * @param v             The vector to be initialized
  * @param data_size     The size of each data element
- * 
+ *
  * @return      The index of the first occurence of the given data, -1 if the
  *              given data does not exist.
  *****************************************************************************/
@@ -138,8 +138,8 @@ int vector_find(vector* v, void* data);
 void vector_clear(vector* v);
 
 /******************************************************************************
- * @brief       Deallocates the memory allocated for the vector
- * 
+ * @brief       Deallocates the memory allocated for the vector.
+ *
  * @return      0 if successful, 1 otherwise
 ******************************************************************************/
 int vector_free(vector* v);
