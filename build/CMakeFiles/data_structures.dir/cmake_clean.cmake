49a051b8e41edc0a6c3295e24258e2240b5fabc1
Luan Truong <truongluan303@gmail.com>
2023-04-02 16:36:06 -0700

    Update queue implementations and tests

---
+++ build/CMakeFiles/data_structures.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/data_structures.dir/src/deque/deque.c.o"
+  "CMakeFiles/data_structures.dir/src/vector/vector.c.o"
+  "libdata_structures.pdb"
+  "libdata_structures.so"
+  "libdata_structures.so.1"
+  "libdata_structures.so.1.0.0"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/data_structures.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- src/deque/deque.h
+++ src/deque/deque.h
@@ -1,16 +1,16 @@
 #ifndef DEQUE_H
 #define DEQUE_H
 
-#define SUCCESS     0
-#define FAILURE     -1
-#define TRUE        1
-#define FALSE       0
+#define DEQUE_SUCCESS   0
+#define DEQUE_FAILURE   -1
+#define DEQUE_TRUE      1
+#define DEQUE_FALSE     0
 
 typedef struct _deque deque;
 
 deque* deque_construct();
 
-int deque_destroy(deque* d);
+int deque_destroy(deque** d);
 
 unsigned int deque_size(deque* d);
 
@@ -27,7 +27,5 @@ void* deque_front(deque* d);
 void* deque_back(deque* d);
 
 int deque_contains(deque* d, void* value);
-
-int deque_remove(deque* d, void* value);
 
 #endif
