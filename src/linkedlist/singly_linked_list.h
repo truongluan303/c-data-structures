2624452c2dbdbc2716c1b14fbe3ee0bc27705b1d
Luan Truong <83048295+truongluan303@users.noreply.github.com>
2023-04-25 01:18:09 -0700

    Add linked list data structure (#5)
    The initial implementation of the linked list data structures along with the test cases for it.

---
+++ src/linkedlist/singly_linked_list.h
@@ -0,0 +1,217 @@
+#ifndef VECTOR_H
+#define VECTOR_H
+
+#define SINGLY_LINKED_LIST_SUCCESS  0
+#define SINGLY_LINKED_LIST_FAILURE  -1
+#define SINGLY_LINKED_LIST_TRUE     1
+#define SINGLY_LINKED_LIST_FALSE    0
+
+/**
+ * @brief Represents the singly linked list.
+ */
+typedef struct _singly_linked_list singly_linked_list;
+
+/**
+ * @brief Represents the singly linked list node.
+ */
+typedef struct _singly_linked_node singly_linked_node;
+
+/**
+ * @brief Construct a new singly linked list.
+ *
+ * @return The newly constructed list.
+ */
+singly_linked_list* sllist_construct();
+
+/**
+ * @brief Free a singly linked list.
+ *
+ * @param list Reference to the singly linked list to be free.
+ * @return SINGLY_LINKED_LIST_SUCCESS if the list is successfully destroyed,
+ *         SINGLY_LINKED_LIST_FAILURE otherwise.
+ */
+int sllist_destroy(singly_linked_list** list);
+
+/**
+ * @brief Get number of nodes in a singly linked list.
+ *
+ * @param list The singly linked list to get the size from.
+ * @return Number of nodes in the given singly linked list.
+ */
+unsigned int sllist_size(singly_linked_list* list);
+
+/**
+ * @brief Add a new node to the end of the given singly linked list.
+ *
+ * @param list The list to append new node to.
+ * @param value The value of the new node.
+ * @return The newly added node, NULL if the operation fails.
+ */
+singly_linked_node* sllist_pushback(singly_linked_list* list, void* value);
+
+/**
+ * @brief Add a new node to the beginning of the given singly linked list.
+ *
+ * @param list The singly linked list to add new node to.
+ * @param value The value of the new node.
+ * @return The newly added node, NULL if the operation fails.
+ */
+singly_linked_node* sllist_pushfront(singly_linked_list* list, void* value);
+
+/**
+ * @brief Add a new node after a given position.
+ *        For example, after appending value 2 to index 1, list `0,1,3` will
+ *        become `0,1,2,3`.
+ *
+ * @param list The singly linked list to add new node to.
+ * @param index The position to append the new node to.
+ * @param value The value of the new node.
+ * @return The newly added node, NULL if the operation fails.
+ */
+singly_linked_node* sllist_insert_after_index(singly_linked_list* list,
+                                              unsigned int index,
+                                              void* value);
+
+/**
+ * @brief Add a new node after the given node.
+ *        For example, after appending value 2 to the first node, list `1,3`
+ *        will become `1,2,3`.
+ *
+ * @param list The singly linked list to add new node to.
+ * @param node The node to append new node to.
+ * @param value The value of the new node.
+ * @return The newly added node, NULL if the operation fails.
+ */
+singly_linked_node* sllist_insert_after_node(singly_linked_list* list,
+                                             singly_linked_node* node,
+                                             void* value);
+
+/**
+ * @brief Add a new node before a given position.
+ *
+ * @param list The singly linked list to add new node to.
+ * @param index The position to prepend the new node to.
+ * @param value The value of the new node.
+ * @return The newly added node, NULL if the operation fails.
+ */
+singly_linked_node* sllist_insert_before_index(singly_linked_list* list,
+                                               unsigned int index,
+                                               void* value);
+
+/**
+ * @brief Get the value of the front (first) node in a singly linked list.
+ *
+ * @param list The singly linked list to get the value from.
+ * @return The value at the front node of the given singly linked list.
+ */
+void* sllist_front_value(singly_linked_list* list);
+
+/**
+ * @brief Get the value of the back (last) node in a singly linked list.
+ *
+ * @param list The singly linked list to get the value from.
+ * @return The value at the back node of the given singly linked list.
+ */
+void* sllist_back_value(singly_linked_list* list);
+
+/**
+ * @brief Get the value of the node at the given position in a singly linked
+ *        list.
+ *
+ * @param list The singly linked list to get the value from.
+ * @param index The position of the node to get value from.
+ * @return The value of the node at the given position.
+ */
+void* sllist_value_at(singly_linked_list* list, unsigned int index);
+
+/**
+ * @brief Get the front (first) node of the singly linked list.
+ *
+ * @param list The singly linked list to get the node from.
+ * @return The front node of the given list, NULL if the operation fails.
+ */
+singly_linked_node* sllist_front_node(singly_linked_list* list);
+
+/**
+ * @brief Get the back (last) node of the singly linked list.
+ *
+ * @param list The singly linked list to get the node from.
+ * @return The back node of the given list, NULL if the operation fails.
+ */
+singly_linked_node* sllist_back_node(singly_linked_list* list);
+
+/**
+ * @brief Get the node at a position in a singly linked list.
+ *
+ * @param list The singly linked list to get the node from.
+ * @param index The position where the node is at.
+ * @return The node at the given position, NULL if the operation fails.
+ */
+singly_linked_node* sllist_node_at(singly_linked_list* list,
+                                   unsigned int index);
+
+/**
+ * @brief Remove the front (first) node from a singly linked list.
+ *
+ * @param list The singly linked list to remove the node from.
+ * @return The value of the removed node.
+ */
+void* sllist_popfront(singly_linked_list* list);
+
+/**
+ * @brief Remove the node at the given position from a singly linked list.
+ *
+ * @param list The singly linked list to remove the node from.
+ * @param index The position of the node to be removed.
+ * @return The value of the removed node.
+ */
+void* sllist_remove_at(singly_linked_list* list, unsigned int index);
+
+/**
+ * @brief Remove the given node's next node from a singly linked list.
+ *
+ * @param list The singly linked list to remove the node from.
+ * @param node The node whose next node will be removed.
+ * @return The value of the removed node.
+ */
+void* sllist_remove_after_node(singly_linked_list* list,
+                               singly_linked_node* node);
+
+/**
+ * @brief Get the value of a node.
+ *
+ * @param node The node to get value from.
+ * @return The value of the given node.
+ */
+void* slnode_value(singly_linked_node* node);
+
+/**
+ * @brief Get the next node of a node.
+ *
+ * @param node The node to get next node from.
+ * @return The next node of the given node.
+ */
+singly_linked_node* slnode_next_node(singly_linked_node* node);
+
+/**
+ * @brief Advance to the next node(s). For example, advanceing 2 steps from node
+ *        1 in list `1,2,3,4` will result in node 3.
+ *
+ * @param node The node to advance from.
+ * @param step_count Number of steps to advance.
+ * @return The node that is advanced to.
+ */
+singly_linked_node* slnode_advance(singly_linked_node* node,
+                                   unsigned int step_count);
+
+/**
+ * @brief Set the value of a node.
+ *
+ * @param node The node whose value to be set.
+ * @param value The new value for the node.
+ * @return SINGLY_LINKED_NODE_SUCCESS if the node is successfully set,
+ *         SINGLY_LINKED_NODE_FALSE otherwise.
+ */
+int slnode_set_value(singly_linked_node* node, void* value);
+
+#endif
---
+++ tests/singly_linked_list-test.c
@@ -0,0 +1,148 @@
+#include "singly_linked_list.h"
+#include "tester/tester.h"
+
+#include <assert.h>
+#include <stdio.h>
+
+
+int main(int argc, char **argv)
+{
+    tester_init();
+
+    singly_linked_list* llist;
+
+    //=========================================================================
+    // TEST 1
+    // Make sure the singly linked list can be constructed successfully.
+    //
+    TRY { llist = sllist_construct(); } ENDTRY;
+
+    TEST_ASSERT(llist != NULL);
+
+    //=========================================================================
+    // TEST 2
+    // Make sure pushback and popfront functions work.
+    //
+    sllist_pushback(llist, (void*)5);
+    sllist_pushback(llist, (void*)6);
+    sllist_pushfront(llist, (void*)4);
+    sllist_pushfront(llist, (void*)3);
+
+    TEST(sllist_size(llist), 4);
+
+    TEST_ASSERT(sllist_front_node(llist) != NULL);
+    TEST_ASSERT(sllist_back_node(llist) != NULL);
+
+    TEST((int)sllist_front_value(llist), 3);
+    TEST((int)sllist_back_value(llist), 6);
+    TEST((int)sllist_value_at(llist, 0), 3);
+    TEST((int)sllist_value_at(llist, 1), 4);
+    TEST((int)sllist_value_at(llist, 2), 5);
+    TEST((int)sllist_value_at(llist, 3), 6);
+    TEST(sllist_node_at(llist, 4), NULL);
+    TEST(sllist_value_at(llist, 4), NULL);
+
+    TEST(slnode_value(sllist_node_at(llist, 0)), 3);
+    TEST(slnode_value(sllist_node_at(llist, 1)), 4);
+    TEST(slnode_value(sllist_node_at(llist, 2)), 5);
+    TEST(slnode_value(sllist_node_at(llist, 3)), 6);
+
+    //=========================================================================
+    // TEST 3
+    // Make sure insert functionalities work.
+    //
+    TEST_ASSERT(sllist_insert_after_index(llist, 3, (void*)9) != NULL);
+    TEST_ASSERT(sllist_insert_after_index(llist, 3, (void*)7) != NULL);
+    TEST_ASSERT(sllist_insert_before_index(llist, 0, (void*)0) != NULL);
+    TEST_ASSERT(sllist_insert_before_index(llist, 1, (void*)2) != NULL);
+
+    TEST(sllist_insert_after_index(llist, 8, (void*)-1), NULL);
+    TEST(sllist_insert_after_index(llist, 9, (void*)-1), NULL);
+
+    TEST_ASSERT(
+        sllist_insert_after_node(llist,
+                                sllist_node_at(llist, 6),
+                                (void*)8) != NULL);
+    TEST_ASSERT(
+        sllist_insert_after_node(llist,
+                                 sllist_front_node(llist),
+                                 (void*)1) != NULL);
+
+    TEST(sllist_insert_before_index(llist, 10, (void*)-1), NULL);
+    TEST(sllist_insert_before_index(llist, 11, (void*)-1), NULL);
+
+    TEST(sllist_size(llist), 10);
+    TEST((int)sllist_front_value(llist), 0);
+    TEST((int)sllist_value_at(llist, 0), 0);
+    TEST((int)sllist_value_at(llist, 1), 1);
+    TEST((int)sllist_value_at(llist, 2), 2);
+    TEST((int)sllist_value_at(llist, 3), 3);
+    TEST((int)sllist_value_at(llist, 4), 4);
+    TEST((int)sllist_value_at(llist, 5), 5);
+    TEST((int)sllist_value_at(llist, 6), 6);
+    TEST((int)sllist_value_at(llist, 7), 7);
+    TEST((int)sllist_value_at(llist, 8), 8);
+    TEST((int)sllist_value_at(llist, 9), 9);
+    TEST((int)sllist_back_value(llist), 9);
+
+    //=========================================================================
+    // TEST 4
+    // Make sure remove functionalities work.
+    //
+    TEST((int)sllist_popfront(llist), 0);
+    TEST((int)sllist_popfront(llist), 1);
+    TEST(sllist_size(llist), 8);
+
+    TEST((int)sllist_remove_at(llist, 0), 2);
+    TEST((int)sllist_remove_at(llist, 2), 5);
+    TEST((int)sllist_remove_at(llist, 4), 8);
+    TEST((int)sllist_remove_at(llist, 4), 9);
+    TEST(sllist_remove_at(llist, 4), NULL);
+    TEST(sllist_remove_at(llist, 5), NULL);
+    TEST(sllist_size(llist), 4);
+
+    TEST((int)sllist_front_value(llist), 3);
+    TEST((int)sllist_value_at(llist, 0), 3);
+    TEST((int)sllist_value_at(llist, 1), 4);
+    TEST((int)sllist_value_at(llist, 2), 6);
+    TEST((int)sllist_value_at(llist, 3), 7);
+    TEST((int)sllist_back_value(llist), 7);
+
+    TEST((int)sllist_remove_after_node(llist, sllist_front_node(llist)), 4);
+    TEST((int)sllist_remove_after_node(llist, sllist_node_at(llist, 1)), 7);
+
+    TEST(sllist_size(llist), 2);
+    TEST((int)sllist_front_value(llist), 3);
+    TEST((int)sllist_value_at(llist, 0), 3);
+    TEST((int)sllist_value_at(llist, 1), 6);
+    TEST((int)sllist_back_value(llist), 6);
+
+    singly_linked_list* empty_list = sllist_construct();
+    TEST(sllist_popfront(empty_list), NULL);
+    TEST(sllist_remove_at(empty_list, 0), NULL);
+
+    //=========================================================================
+    // TEST 5
+    // Make sure set node value functionality works
+    //
+    TEST(slnode_set_value(sllist_node_at(llist, 0), (void*)10),
+         SINGLY_LINKED_LIST_SUCCESS);
+    TEST(slnode_set_value(sllist_node_at(llist, 1), (void*)11),
+         SINGLY_LINKED_LIST_SUCCESS);
+
+    TEST((int)sllist_front_value(llist), 10);
+    TEST((int)sllist_value_at(llist, 0), 10);
+    TEST((int)sllist_value_at(llist, 1), 11);
+    TEST((int)sllist_back_value(llist), 11);
+
+    //=========================================================================
+    // TEST 6
+    // Make sure singly linked list is successfully destroyed.
+    //
+    TEST(sllist_destroy(&llist), SINGLY_LINKED_LIST_SUCCESS);
+    TEST(sllist_destroy(&empty_list), SINGLY_LINKED_LIST_SUCCESS);
+    TEST(llist, NULL);
+
+    tester_report(stdout, argv[0]);
+    return tester_result();
+}
